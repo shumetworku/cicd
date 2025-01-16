88748f420d40ab3b15ab9e3ff3f1acecc45eb420
shumetworku <40451277+shumetworku@users.noreply.github.com>
2025-01-16 15:07:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 'use strict';
 
-const Hapi   = require('@hapi/hapi');
-
-const Server = new Hapi.Server({
+const Hapi = require('@hapi/hapi');
+const Server = Hapi.server({ // Corrected the initialization
     host: 'localhost',
     port: 3000
 });
-const Hello  = require('./lib/hello');
+const Hello = require('./lib/hello');
 
 Server.route({
     method: 'GET',
     path: '/hello/{user}',
-    handler: function (request, reply) {
-
+    handler: (request, h) => {
         const result = Hello(decodeURIComponent(request.params.user));
         return result;
     }
 });
 
-// don't start server if this file was required
-
+// Don't start the server if this file is required
 if (!module.parent) {
-
-    Server.start((err) => {
-
-        if (err) {
-            throw err;
+    const start = async () => {
+        try {
+            await Server.start();
+            console.log(`Server running at: ${Server.info.uri}`);
+        } catch (err) {
+            console.error(err);
+            process.exit(1);
         }
-
-        console.log(`Server running at: ${Server.info.uri}`);
-    });
+    };
+    start();
 }
 
 module.exports = Server;
